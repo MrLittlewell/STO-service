8c03a6ac4d52dd008831d805bed9482fdc37d0e9
Ruslan Littlewell Home <mrlittlewell@gmail.com>
2019-11-29 03:28:34 +0300

    update

--- js/init.js
+++ js/init.js
@@ -74,3 +74,41 @@ $(function(){
 		}
 	});
 });
+
+var principleSlider = new Swiper('.principle-slider', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  nextButton: '.principle-next',
+  prevButton: '.principle-prev',
+  paginationClickable: true,
+  loop: true,
+  autoplay: 1000,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    1150: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+
+});
+  $('.principle-next').click(function(){
+    principleSlider.slideNext();
+  });
+  $('.principle-prev').click(function() {
+    principleSlider.slidePrev();
+  });
